0a7e43d33419d8999b4324d3dcd6b8ef8b919429
taeyang121096 <taeyang121096@naver.com>
2021-08-22 05:03:59 +0900

    upd: test/.../MemberServiceTest -> update(add) duplicate(upd)

--- server/item/src/test/java/shop/item/service/MemberServiceTest.java
+++ server/item/src/test/java/shop/item/service/MemberServiceTest.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+import shop.item.domain.Address;
 import shop.item.domain.Member;
 import shop.item.repository.MemberRepository;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,27 +23,28 @@ class MemberServiceTest {
     @Autowired MemberService memberService;
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
 
     @Test
     void 회원가입() throws Exception {
         //given
-        Member member = new Member();
-//        member.setUserName("aa");
-
+        Address address = new Address("광주","1596-15","3564");
+        Member member = Member.createMember("sunny","lso","sunny","tae","010-0000-0000",address);
         //when
-        Long join = memberService.join(member);
+        Long saveId = memberService.join(member);
         //then
-        assertEquals(member, memberRepository.findOne(join));
+        assertEquals(member, memberRepository.findOne(saveId));
     }
 
     @Test
     void 중복_아이디() throws Exception {
         //given
-        Member member1 = new Member();
-//        member1.setUserId("aa");
+        Address address = new Address("광주","1596-15","3564");
+        Member member1 = Member.createMember("sun","lso","sunny","tae","010-0000-0000",address);
 
-        Member member2 = new Member();
-//        member2.setUserId("aa");
+        Member member2 = Member.createMember("sun","lso","sunny","tae","010-0000-0000",address);
+
         //when
 
         //then
@@ -48,6 +52,23 @@ class MemberServiceTest {
             memberService.join(member1);
             memberService.join(member2);
         });
+    }
+
+    @Test
+    void 정보_변경() throws Exception{
+        //given
+        Address address = new Address("광주","1596-15","3564");
+        Member member = Member.createMember("sunny","lso","sunny","tae","010-0000-0000",address);
+        Long id = memberService.join(member);
+        //when
+        Member find = memberService.findOne(id);
+        em.flush();
+        em.clear();
+        memberService.update(find.getId(),"lsolyh",find.getNickName(),find.getAddress());
+        Member update = memberService.findOne(id);
+
+        //then
+        assertNotEquals(find.getUserPw(),update.getUserPw());
     }
 
 
